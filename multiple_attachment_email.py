a4a9730ee36ad54ca352fd4c9704ffb2856668ca
weipanchang <weipanchang@hotmail.com>
2014-07-28 15:58:42 -0700

    Clean the code

--- multiple_attachment_email.py
+++ multiple_attachment_email.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 
-#import smtplib
-from smtplib import SMTP_SSL as SMTP 
+from smtplib import SMTP_SSL as SMTP
 import base64
 import urllib2
 import sys
@@ -11,8 +10,6 @@ import re
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
-#from smtplib import SMTP
-
 
 FILENAME1="Cups.mp3"
 FILENAME2="test.bat"
@@ -29,23 +26,22 @@ msg['Subject'] = 'Email with Attachemnt'
 msg['From'] = FROMADDR
 msg['Reply-to'] = LOGIN
 
-# Read a file and encode it into base64 format
 server = SMTP(SMTPserver)
 server.set_debuglevel(1)
 server.ehlo()
-#server.starttls()
+
 server.login(LOGIN, PASSWORD)
 part = MIMEText("Hello im sending an email from a python program")
 msg.attach(part)
+
 msg.preamble = 'Multipart massage.\n'
+
+# Read a file and encode it into base64 format
 for FILENAME in FILE:
-
-# This is the textual part:
-
 # This is the binary part(The Attachment):
     part = MIMEApplication(open(FILENAME,"rb").read())
     part.add_header('Content-Disposition', 'attachment', filename=FILENAME)
     msg.attach(part)
-    
+
 server.sendmail(FROMADDR, TOADDRS, msg.as_string())
 server.quit()
